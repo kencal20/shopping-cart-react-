1cb5961ba2051a5d993a4ce8bbaa12d9bfadb613
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-06-20 11:41:24 +0000

    content file creation & added in app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from  './Components/Navbar'
 import Side_bar from './Components/Side_bar'
+import Content from  './Components/Content'
 function App() {
   return (
   <>
   <Navbar/>
   <Side_bar/>
+  <Content/>
   </>
   );
 }
