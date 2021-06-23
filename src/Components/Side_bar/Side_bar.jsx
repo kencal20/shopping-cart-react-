32218b5e5b87ddcfc27c8ba358eec727655871de
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-06-23 19:16:53 +0000

    side_bar files

---
+++ src/Components/Side_bar/Side_bar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import side_bar  from './../Side_bar/Side_bar.css'
+
+const Side_bar = () => {
+    return (
+  
+        <div class="col-md-2 size-column size-circle ">
+        <div class="col-md-3"><p>xs</p></div>
+        <div class="col-md-3"><p>S</p></div>
+        <div class="col-md-3"><p>M</p></div>   
+        <div class="col-md-3"><p>L</p></div>
+        <div class="col-md-3"> <p>Xl</p></div>
+        <div class="col-md-3"><p>XXL</p></div><br></br>
+        
+         <br></br>  <span class="glyphicon glyphicon-star glyphicon" aria-hidden="true"></span> Star
+        
+        
+       
+        
+        </div>
+    );
+}
+
+export default Side_bar;
