84800e64f014e5a554e7f56ee1cb3456eb185727
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-07-11 09:23:01 +0000

    import link change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from  './Components/Navbar/Navbar'
 import Side_bar from './Components/Side_bar/Side_bar'
-import Content from  './Components/Body/Content'
+import Content from  './Components/Content/Content'
 function App() {
   return (
   <>
