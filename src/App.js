174d67c1263cfae29cce45fe8a3b7759951ba23a
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-07-11 09:24:42 +0000

    format page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Navbar from  './Components/Navbar/Navbar'
-import Side_bar from './Components/Side_bar/Side_bar'
-import Content from  './Components/Content/Content'
+import Navbar from "./Components/Navbar/Navbar";
+import Side_bar from "./Components/Side_bar/Side_bar";
+import Content from "./Components/Content/Content";
 function App() {
   return (
-  <>
-  <Navbar/>
-  <Side_bar/>
-  <Content/>
-  </>
+    <>
+      <Navbar />
+      <Side_bar />
+      <Content />
+    </>
   );
 }
 
